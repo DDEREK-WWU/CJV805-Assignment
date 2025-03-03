76365e2474eaff09aaec5b5e6506d17386abdbab
Derek Wu <dwu89@myseneca.ca>
2025-03-03 15:53:32 -0500

    revert to fetching localhost data

--- src/context/MediaContext.js
+++ src/context/MediaContext.js
@@ -3,18 +3,17 @@ import React, { createContext, useState, useEffect } from 'react';
 export const MediaContext = createContext();
 
 
-const API_BASE_URL = "../api"; // Now points to the Vercel serverless function
 
 const MediaProvider = ({ children }) => {
   const [movies, setMovies] = useState([]);
   const [tvShows, setTvShows] = useState([]);
 
   useEffect(() => {
-    fetch(`https://cjv-805-assignment-git-main-dderek-wwus-projects.vercel.app/api/movies`)
+    fetch(`http://localhost:3001/api/movies`)
       .then(response => response.json())
       .then(data => setMovies(data));
 
-    fetch(`https://cjv-805-assignment-git-main-dderek-wwus-projects.vercel.app/api/tvShows`)
+    fetch(`http://localhost:3001/api/tvshows`)
       .then(response => response.json())
       .then(data => setTvShows(data));
   }, []);
