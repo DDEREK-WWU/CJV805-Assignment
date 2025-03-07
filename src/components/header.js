c6bffecf722a6e9c88c57d533b11736c628337d8
Derek Wu <dwu89@myseneca.ca>
2025-03-07 16:22:28 -0500

    updated header logo link

--- src/components/header.js
+++ src/components/header.js
@@ -12,18 +12,18 @@ const Header = () => {
         <AppBar position="fixed" sx={{ backgroundColor: darkMode ? "#222" : "#3d3838", color: darkMode ? "#fff" : "#3d3838" }}>
             <Toolbar sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", flexWrap: "wrap" }}>
                 
-                {/* Logo Section */}
-                <Box display="flex" alignItems="center">
-                    <IconButton component={Link} to="/">
-                        <LiveTvIcon sx={{ color: "green" }} fontSize="large" />
-                    </IconButton>
-                    <Typography variant="h6" sx={{ fontWeight: "bold", ml: 1 ,color: "#fff"}}>
+            {/* Logo Section */}
+            <Box display="flex" alignItems="center">
+                <Link to="/" style={{ display: "flex", alignItems: "center", textDecoration: "none", color: "inherit" }}>
+                    <LiveTvIcon sx={{ color: "green" }} fontSize="large" />
+                    <Typography variant="h6" sx={{ fontWeight: "bold", ml: 1, color: "#fff", mt:1 }}>
                         NetHuDu
                     </Typography>
-                </Box>
+                </Link>
+            </Box>
 
                 {/* Navigation Section */}
-                <Box sx={{ display: "flex", alignItems: "center", gap: 2, flexWrap: "wrap" }}>
+                <Box sx={{ display: "flex", alignItems: "center", gap: 2, flexWrap: "wrap", color: "#fff"}}>
                     <Link to="/movies&tvshows" className="nav-link">Movies/TV Shows</Link>
                     <Link to="/signup" className="nav-link">Sign Up</Link>
                     <Link to="/login" className="nav-link">Login</Link>
